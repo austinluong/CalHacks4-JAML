5395143c9ddbcc8c0fbe73802548aa98af58837e
Jerry <jerry.j.jin@berkeley.edu>
2017-10-07 23:37:02 -0700

    Removed deprecated variables. Empty for now.

--- analyze/util.py
+++ analyze/util.py
@@ -1,11 +1,1 @@
 """File for holding global variables and other utilities."""
-
-ARTICLE_NUM = 100
-
-SAVE_NUM = 10
-
-SOURCES = {'cnn': 'http://cnn.com', 
-	'nbc': 'http://nbcnews.com', 
-	'bbc': 'http://bbcnews.com',
-	'fox': 'http://foxnews.com', 
-	'nyt': 'http://nytimes.com' }
