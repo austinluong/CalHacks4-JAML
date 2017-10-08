a0f01571f6dfe0108e9a3f0622fa32b1f3d85530
Laney Huang <lh1696@berkeley.edu>
2017-10-08 05:47:56 -0700

    stuff

--- expo/screens/HomeScreen.js
+++ expo/screens/HomeScreen.js
@@ -7,91 +7,69 @@ import {
   Text,
   TouchableOpacity,
   View,
+  AppRegistry,
 } from 'react-native';
+import { 
+  StackNavigator,
+} from 'react-navigation';
 import { 
   WebBrowser,
 } from 'expo';
 
 import { MonoText } from '../components/StyledText';
 
-
 class NewsEntry extends React.Component {
   render() {
     return (
-      <View style={styles.newsEntry}>
-        <View style={{flexDirection: 'row'}}>
-          
-          {/*Left*/}
+      <TouchableOpacity style={styles.newsEntry} onPress={()=> {
+        // navigate('Page');
+        }}>
+      <View>
           <View>
-            <View style={styles.container}>
             <Image
               source={
                   require('../assets/images/robot-dev.png')
               }
             />
-            </View> 
-            <Text style={styles.sourceLeft}>Source</Text>
-            <Text style={styles.timeLeft}>Hours</Text>
-            <Text style={styles.titleLeft}>Title</Text>
-            <View style={styles.shareContainer}>
-              <View>
-                <Image
-                  source={
-                      require('../assets/images/share.png')            
-                  }
-                  style={styles.littlebuttons}
-                />
-              </View>
-              <View>
-                <Image
-                  source={
-                      require('../assets/images/bookmark.png')            
-                  }
-                  style={styles.littlebuttons}
-                />
-              </View>
-            </View>             
+          </View> 
+          <Text style={styles.source}>Source</Text>
+          <Text style={styles.time}>Hours</Text>
+          <Text style={styles.title}>Title</Text>
+          <View style={styles.shareContainer}>
+            <View>
+              <Image
+                source={
+                    require('../assets/images/share.png')            
+                }
+                style={styles.littlebuttons}
+              />
+            </View>
+            <View>
+              <Image
+                source={
+                    require('../assets/images/bookmark.png')            
+                }
+                style={styles.littlebuttons}
+              />
+            </View>
           </View>
-          
-          <View style={styles.spacer}></View>
-
-          {/*Right*/}
-          <View style={styles.container}>
-            <Image
-              source={
-                  require('../assets/images/robot-dev.png')
-              }
-            />
-            <Text style={styles.sourceRight}>Source</Text>
-            <Text style={styles.timeRight}>Hours</Text>
-            <Text style={styles.titleRight}>Title</Text>
-            <View style={styles.shareContainer}>
-              <View>
-                <Image
-                  source={
-                      require('../assets/images/share.png')            
-                  }
-                  style={styles.littlebuttons}
-                />
-              </View>
-              <View>
-                <Image
-                  source={
-                      require('../assets/images/bookmark.png')            
-                  }
-                  style={styles.littlebuttons}
-                />
-              </View>
-            </View>             
-          </View>
-
-        
-        </View>
       </View>
+    </TouchableOpacity>
     )
   }
 }
 
+class NewsEntryRow extends React.Component {
+  render() {
+    return (
+      <View style={styles.newsEntryRow}>
+        <NewsEntry></NewsEntry>        
+        <View style={styles.spacer}></View>
+        <NewsEntry></NewsEntry>
+      </View>
+    )
+  }
+} 
 
 export default class HomeScreen extends React.Component {
   render() {
@@ -123,8 +101,8 @@ export default class HomeScreen extends React.Component {
                 borderBottomWidth: 1,
               }}
             />
-            <NewsEntry></NewsEntry>
-            <NewsEntry></NewsEntry>
+            <NewsEntryRow></NewsEntryRow>
+            <NewsEntryRow></NewsEntryRow>
           </View>
         </ScrollView>
       </View>
@@ -168,39 +146,18 @@ const styles = StyleSheet.create({
   spacer: {
     flex: 0.1,
   },
-  sourceLeft: {
-    flex: 0.4,
+  source: {
     fontSize: 14,
     fontFamily: 'circular-medium',
     color: 'rgb(90,182,161)',
   },
-  sourceRight: {
-    flex: 0.4,
-    fontSize: 14,
-    fontFamily: 'circular-medium',
-    color: 'rgb(90,182,161)',
-  },
-  timeLeft: {
-    flex: 0.4,
+  time: {
     fontSize: 12,
     fontFamily: 'circular-book',
   },
-  timeRight: {
-    flex: 0.4,
-    fontSize: 12,
-    fontFamily: 'circular-book',
-  },
-  titleLeft: {
+  title: {
     paddingTop: 10, 
     paddingBottom: 10,
-    flex: 0.4,
-    fontSize: 18,
-    fontFamily: 'circular-book',
-  },
-  titleRight: {
-    paddingTop: 10,
-    paddingBottom: 10,
-    flex: 0.4,
     fontSize: 18,
     fontFamily: 'circular-book',
   },
@@ -217,8 +174,12 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
 
   },
-  newsEntry: {
+  newsEntryRow: {
     paddingTop: 20,
     paddingBottom: 20,
+    flexDirection: 'row',
+  },
+  newsEntry: {
+    flex: 0.4,
   }
 });
